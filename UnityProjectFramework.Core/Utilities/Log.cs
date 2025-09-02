a5962a420d1797d1624912bbf2d1e5b3f9b3cbfc
HuiSung <gmltjd1503@gmail.com>
2025-09-02 18:19:57 +0900

    Log.cs

--- UnityProjectFramework.Core/Utilities/Log.cs
+++ UnityProjectFramework.Core/Utilities/Log.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Diagnostics;
 using Debug = UnityEngine.Debug;
 
@@ -6,6 +7,13 @@ namespace UnityProjectFramework.Core
 {
     public static class Log
     {
+        #region Const & Struct
+
+        private const string INFO_COLOR = "<color=#56F8FF>";
+        private const string WARNING_COLOR = "<color=#FFFD56>";
+        private const string ERROR_COLOR = "<color=#FF6756>";
+        private const string COLOR_END = "</color>";
+        
         public enum LoggingLevel
         {
             Info,
@@ -14,27 +22,31 @@ namespace UnityProjectFramework.Core
             Exception
         }
 
+        #endregion
+
         public static LoggingLevel Level { get; private set; } = LoggingLevel.Error;
         public static void SetLevel(LoggingLevel level)
         {
             Level = level;
         }
-        
+
+        #region Level #Debug
+
         [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
-        public static void D(string message)
+        public static void D(object message)
         {
             if (Level >= LoggingLevel.Info)
             {
-                Debug.Log($"");
+                Debug.Log($"{INFO_COLOR}[INFO]{COLOR_END} {message}");
             }
         }
         
         [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
-        public static void D(string message, string hexColor)
+        public static void D(object message, string hexColor)
         {
             if (Level >= LoggingLevel.Info)
             {
-                Debug.Log($"");
+                Debug.Log($"{INFO_COLOR}[INFO]{COLOR_END} <color={hexColor}>{message}</color>");
             }
         }
         
@@ -43,16 +55,20 @@ namespace UnityProjectFramework.Core
         {
             if (Level >= LoggingLevel.Info)
             {
-                Debug.Log($"");
+                Debug.LogFormat($"{INFO_COLOR}[INFO]{COLOR_END} {formattedMessage}", args);
             }
         }
 
+        #endregion
+
+        #region Level #Warning
+
         [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
-        public static void W(string message)
+        public static void W(object message)
         {
             if(Level >= LoggingLevel.Warning)
             {
-                Debug.LogWarning($"");
+                Debug.LogWarning($"{WARNING_COLOR}[INFO]{COLOR_END} {message}");
             }
         }
         
@@ -61,20 +77,41 @@ namespace UnityProjectFramework.Core
         {
             if(Level >= LoggingLevel.Warning)
             {
-                Debug.LogWarning($"");
+                Debug.LogWarningFormat($"{WARNING_COLOR}[INFO]{COLOR_END} {formattedMessage}", args);
+            }
+        }
+
+        #endregion
+
+        #region Level #Error & Exception
+
+        [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
+        public static void E(object message)
+        {
+            if (Level >= LoggingLevel.Error)
+            {
+                Debug.LogError($"{ERROR_COLOR}[INFO]{COLOR_END} {message}");
+            }
+        }
+        
+        [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
+        public static void E(string formattedMessage, params object[] args)
+        {
+            if (Level >= LoggingLevel.Error)
+            {
+                Debug.LogErrorFormat($"{ERROR_COLOR}[INFO]{COLOR_END} {formattedMessage}", args);
             }
         }
 
         [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
-        public static void E()
+        public static void Ex(Exception exception)
         {
-            
+            if (Level >= LoggingLevel.Exception)
+            {
+                Debug.LogException(exception);
+            }
         }
 
-        [Conditional(Pdv.SYMBOL_LOG_ENABLED)]
-        public static void Ex()
-        {
-            
-        }
+        #endregion
     }
 }
